5f50710964dfeff7477ec04b6c593e47f143babb
riflowth <riflowth@gmail.com>
2023-06-23 23:38:47 +0700

    refactor: add color variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,49 @@
+const typographyPlugin = require('@tailwindcss/typography');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Sarabun', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        danger: {
+          DEFAULT: 'hsl(var(--danger))',
+          foreground: 'hsl(var(--danger-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+
+        border: 'hsl(var(--border))',
+        ring: 'hsl(var(--ring))',
+        input: 'hsl(var(--input))',
+      },
+    },
   },
-  plugins: [],
+  plugins: [typographyPlugin],
 };
