88a38328cc8114c2a727946b618a7dc21f0373ce
riflowth <riflowth@gmail.com>
2023-08-19 21:12:17 +0700

    refactor: add private route component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import '@/assets/css/global.css';
+import { PrivateRoute } from '@/components/common/PrivateRoute';
 import { Toaster } from '@/components/common/Toaster';
 
 import { DashboardPage } from '@/pages/DashboardPage';
 import { SignInPage } from '@/pages/SignInPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <SignInPage />,
-  },
-  {
-    path: '/dashboard',
-    element: <DashboardPage />,
-  },
-]);
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      {/* Router */}
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes. Authentication is not required */}
+          <Route
+            path="/"
+            element={<SignInPage />}
+          />
+
+          {/* Private routes. Authentication is required */}
+          <Route
+            path="/dashboard"
+            element={<PrivateRoute element={<DashboardPage />} />}
+          />
+        </Routes>
+      </BrowserRouter>
+
+      {/* Addons */}
       <Toaster />
     </QueryClientProvider>
   );
