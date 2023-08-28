3cdc36fb819a1c1b894a330c8651ede8e4b87399
riflowth <riflowth@gmail.com>
2023-08-28 16:02:54 +0700

    build: add `vite-plugin-svgr` to import svg directly

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
