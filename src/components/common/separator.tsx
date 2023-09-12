82fbbe44174970c4d32f265b9cca6a1c2f8e9222
riflowth <riflowth@gmail.com>
2023-09-12 16:30:28 +0700

    refactor: add base components

---
+++ src/components/common/separator.tsx
@@ -0,0 +1,21 @@
+import { classNames } from '@/libs/utils';
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import { forwardRef } from 'react';
+
+export const Separator = forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(({ className, orientation = 'horizontal', decorative = true, ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    ref={ref}
+    decorative={decorative}
+    orientation={orientation}
+    className={classNames(
+      'shrink-0 bg-border',
+      orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+      className,
+    )}
+    {...props}
+  />
+));
+Separator.displayName = SeparatorPrimitive.Root.displayName;
