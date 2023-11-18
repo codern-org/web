4ce4b79bb72dc451bea753606b6b701302d33e7e
riflowth <riflowth@gmail.com>
2023-11-19 00:47:40 +0700

    refactor: redesign workspace card

--- src/components/features/dashboard/card-skeleton.tsx
+++ src/components/features/dashboard/card-skeleton.tsx
@@ -1,22 +1,32 @@
+import { Button } from '@/components/common/button';
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/common/card';
 import { Skeleton } from '@/components/common/skeleton';
+import { MoreVerticalIcon } from 'lucide-react';
 
 export const WorkspaceCardSkeleton = () => {
   return (
     <Card>
-      <CardHeader className="flex-row items-center space-x-3">
-        <Skeleton className="h-10 w-10 flex-none bg-muted-foreground" />
-        <div className="mb-1 flex flex-col space-y-2">
-          <Skeleton className="h-5 w-12" />
-          <Skeleton className="h-3 w-24" />
+      <CardHeader className="flex flex-row justify-between">
+        <div className="flex flex-row items-center space-x-3">
+          <Skeleton className="h-10 w-10 flex-none bg-muted-foreground" />
+          <Skeleton className="h-5 w-40" />
         </div>
+        <Button
+          className="p-2 text-muted-foreground"
+          variant="ghost"
+          disabled
+        >
+          <MoreVerticalIcon className="h-5 w-5" />
+        </Button>
       </CardHeader>
       <CardContent className="flex items-center space-x-2">
         <Skeleton className="h-6 w-12" />
         <Skeleton className="h-6 w-12" />
+        <Skeleton className="h-1 flex-1" />
       </CardContent>
-      <CardFooter>
-        <Skeleton className="h-1 w-full bg-muted" />
+      <CardFooter className="flex justify-between">
+        <Skeleton className="h-4 w-32" />
+        <Skeleton className="h-4 w-12" />
       </CardFooter>
     </Card>
   );
--- src/components/features/dashboard/card.tsx
+++ src/components/features/dashboard/card.tsx
@@ -1,9 +1,23 @@
+import { Badge } from '@/components/common/badge';
+import { Button } from '@/components/common/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/common/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/common/dropdown';
 import { Image } from '@/components/common/image';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/common/tooltip';
 import { RoutePath } from '@/libs/constants';
-import { compactNumber } from '@/libs/utils';
-import { Workspace } from '@/types/workspace-type';
-import { BookOpenIcon, Users2Icon } from 'lucide-react';
+import { compactNumber, formatDate } from '@/libs/utils';
+import { Workspace, WorkspaceRole } from '@/types/workspace-type';
+import { BookOpenIcon, MoreVerticalIcon, StarIcon, Users2Icon } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 type WorkspaceCardProps = {
@@ -17,55 +31,92 @@ export const WorkspaceCard = ({ workspace }: WorkspaceCardProps) => {
     id,
     name,
     profileUrl,
-    ownerName,
     totalAssignment,
     completedAssignment,
+    role,
     participantCount,
+    joinedAt,
   } = workspace;
+  const progress = totalAssignment ? (completedAssignment / totalAssignment) * 100 : 0;
 
   return (
     <Card
       className="bg-background transition-colors hover:border-primary"
-      onClick={() => navigate(RoutePath.WORKSPACE.replace(':workspaceId', id.toString()))}
+      onClick={() => {
+        if (role === WorkspaceRole.MEMBER) {
+          navigate(RoutePath.WORKSPACE.replace(':workspaceId', id.toString()));
+        } else {
+          navigate(RoutePath.ORGANIZER_WORKSPACE.replace(':workspaceId', id.toString()));
+        }
+      }}
     >
-      <CardHeader className="flex-row items-center space-x-3">
-        <Image
-          src={profileUrl}
-          className="h-10 w-10 flex-none rounded-md"
-          alt=""
-        />
-        <div className="mb-1 flex flex-col">
-          <CardTitle className="text-base">{name}</CardTitle>
-          <span className="text-xs text-muted-foreground">{ownerName}</span>
+      <CardHeader className="flex flex-row justify-between">
+        <div className="flex flex-row items-center space-x-3">
+          <Image
+            src={profileUrl}
+            className="h-10 w-10 flex-none rounded-md"
+            alt=""
+          />
+          <CardTitle className="text-lg">{name}</CardTitle>
         </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="p-2 text-muted-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+              variant="ghost"
+            >
+              <MoreVerticalIcon className="h-5 w-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-36">
+            <DropdownMenuItem>
+              <StarIcon className="mr-2 h-4 w-4" />
+              <span>Favorite</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </CardHeader>
-      <CardContent className="flex items-center space-x-2">
-        <div className="inline-flex items-center space-x-1 rounded-md bg-muted px-2 py-0.5 text-muted-foreground">
-          <BookOpenIcon className="h-4 w-4" />
-          <span className="text-sm">
-            {totalAssignment === 0 ? (
-              '-'
-            ) : (
-              <>
-                {completedAssignment} / {totalAssignment}
-              </>
-            )}
-          </span>
-        </div>
-        <div className="inline-flex items-center space-x-1 rounded-md bg-muted px-2 py-0.5 text-muted-foreground">
+      <CardContent className="flex space-x-3">
+        <Badge
+          variant="muted"
+          className="space-x-1 rounded-md font-normal"
+        >
           <Users2Icon className="h-5 w-5" />
           <span className="text-sm">{compactNumber(participantCount || 0)}</span>
-        </div>
+        </Badge>
+        <Badge
+          variant="muted"
+          className="space-x-1 rounded-md font-normal"
+        >
+          <BookOpenIcon className="h-4 w-4" />
+          <span className="text-sm">{totalAssignment || 0}</span>
+        </Badge>
+        {role === WorkspaceRole.MEMBER && (
+          <TooltipProvider delayDuration={300}>
+            <Tooltip>
+              <TooltipTrigger className="flex w-full items-center space-x-3">
+                <div className="h-1 w-full rounded bg-muted">
+                  <div
+                    className="h-1 rounded bg-primary"
+                    style={{ width: progress + '%' }}
+                  />
+                </div>
+                <div className="text-xs">{progress}%</div>
+              </TooltipTrigger>
+              <TooltipContent>
+                Assignment progession: {completedAssignment}/{totalAssignment}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
       </CardContent>
-      <CardFooter>
-        <div className="h-1 w-full rounded bg-muted">
-          <div
-            className="h-1 rounded bg-primary"
-            style={{
-              width: (totalAssignment ? (completedAssignment / totalAssignment) * 100 : 0) + '%',
-            }}
-          />
+      <CardFooter className="flex justify-between">
+        <div className="text-xs text-muted-foreground">
+          Joined at: {formatDate(new Date(joinedAt), 'dd MMM yyyyy')}
         </div>
+        {role !== WorkspaceRole.MEMBER && (
+          <Badge className="capitalize">{role.toLowerCase()}</Badge>
+        )}
       </CardFooter>
     </Card>
   );
