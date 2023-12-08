acdb75c8c3b07b0d37c728632ab83ec6f64b50c5
riflowth <riflowth@gmail.com>
2023-12-08 14:45:03 +0700

    refactor: different data according to user role

--- src/components/features/workspace/header.tsx
+++ src/components/features/workspace/header.tsx
@@ -4,7 +4,8 @@ import { Skeleton } from '@/components/common/skeleton';
 import { WorkspaceBreadcrumb } from '@/components/features/workspace/breadcrumb';
 import { useGetWorkspaceQuery } from '@/hooks/workspace-hook';
 import { formatDate } from '@/libs/utils';
-import { CalendarIcon, UserIcon } from 'lucide-react';
+import { WorkspaceRole } from '@/types/workspace-type';
+import { CalendarIcon } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 
 export const WorkspaceHeader = () => {
@@ -17,31 +18,50 @@ export const WorkspaceHeader = () => {
         <WorkspaceBreadcrumb className="pt-6" />
 
         <div className="flex items-center justify-between space-x-8 py-8">
-          <div className="flex flex-col space-y-12">
-            <div className="flex items-center space-x-3">
-              <Image
-                src={workspace?.profileUrl}
-                alt=""
-                className="h-11 w-11 rounded-md"
-              />
-              {workspace ? (
-                <h2 className="text-4xl font-semibold tracking-tight">{workspace.name}</h2>
-              ) : (
-                <Skeleton className="h-9 w-48" />
-              )}
-            </div>
-            <div className="flex items-center space-x-6">
+          <div className="flex items-center space-x-3">
+            <Image
+              src={workspace?.profileUrl}
+              alt=""
+              className="h-11 w-11 rounded-md"
+            />
+            {workspace ? (
+              <h2 className="text-4xl font-semibold tracking-tight">{workspace.name}</h2>
+            ) : (
+              <Skeleton className="h-9 w-48" />
+            )}
+          </div>
+
+          <div className="relative hidden overflow-hidden rounded-md border bg-gradient-to-br from-background/50 from-60% to-accent/50 shadow-lg lg:block">
+            <MojiBun className="absolute -bottom-4 right-8 z-10 h-24 w-24" />
+            <div className="relative z-20 grid grid-cols-2 gap-x-6 gap-y-8 bg-accent/20 p-6">
               <div>
-                <p className="mb-1 flex items-center text-sm leading-none text-muted-foreground">
-                  <UserIcon className="mr-1 h-4 w-4" />
-                  Owner
-                </p>
                 {workspace ? (
-                  <p className="text-sm font-medium">{workspace.ownerName}</p>
+                  <p className="mb-1 text-xl font-medium">{workspace.totalAssignment}</p>
                 ) : (
-                  <Skeleton className="mt-2 h-3 w-24" />
+                  <Skeleton className="mt-1 h-7 w-10" />
                 )}
+                <p className="text-sm text-muted-foreground">Total assignments</p>
               </div>
+              {workspace ? (
+                workspace.role === WorkspaceRole.MEMBER ? (
+                  <div>
+                    <p className="mb-1 text-xl font-medium">
+                      {workspace.totalAssignment - workspace.completedAssignment}
+                    </p>
+                    <p className="text-sm text-muted-foreground">Incompleted assignments</p>
+                  </div>
+                ) : (
+                  <div>
+                    <p className="mb-1 text-xl font-medium">{workspace.participantCount}</p>
+                    <p className="text-sm text-muted-foreground">People in workspace</p>
+                  </div>
+                )
+              ) : (
+                <div>
+                  <Skeleton className="mb-2 h-7 w-10" />
+                  <Skeleton className="h-4 w-44" />
+                </div>
+              )}
               <div>
                 <p className="mb-1 flex items-center text-sm leading-none text-muted-foreground">
                   <CalendarIcon className="mr-1 h-4 w-4" />
@@ -52,40 +72,8 @@ export const WorkspaceHeader = () => {
                     {formatDate(workspace.joinedAt, 'EEEE, d MMMM yyyy')}
                   </p>
                 ) : (
-                  <Skeleton className="mb-1 h-3 w-48" />
+                  <Skeleton className="mt-2 h-4 w-48" />
                 )}
-              </div>
-            </div>
-          </div>
-
-          <div className="relative hidden overflow-hidden rounded-md border bg-gradient-to-br from-background/50 from-60% to-accent/50 shadow-lg lg:block">
-            <MojiBun className="absolute -bottom-4 right-8 z-10 h-24 w-24" />
-            <div className="relative z-20 grid grid-cols-2 gap-6 bg-accent/20 p-6">
-              <div>
-                {workspace ? (
-                  <p className="mb-1 text-xl font-medium">{workspace.totalAssignment}</p>
-                ) : (
-                  <Skeleton className="mb-1 h-7 w-10" />
-                )}
-                <p className="text-sm text-muted-foreground">Total assignments</p>
-              </div>
-              <div>
-                {workspace ? (
-                  <p className="mb-1 text-xl font-medium">
-                    {workspace.totalAssignment - workspace.completedAssignment}
-                  </p>
-                ) : (
-                  <Skeleton className="mb-1 h-7 w-10" />
-                )}
-                <p className="text-sm text-muted-foreground">Incompleted assignments</p>
-              </div>
-              <div>
-                {workspace ? (
-                  <p className="mb-1 text-xl font-medium">{workspace.participantCount}</p>
-                ) : (
-                  <Skeleton className="mb-1 h-7 w-10" />
-                )}
-                <p className="text-sm text-muted-foreground">People in workspace</p>
               </div>
             </div>
           </div>
