e68ef6766f97f54be10d3e20646c0991924397dd
riflowth <riflowth@gmail.com>
2024-03-01 01:09:41 +0700

    refactor: remove unused props

--- src/components/common/calendar.tsx
+++ src/components/common/calendar.tsx
@@ -49,8 +49,8 @@ export function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
