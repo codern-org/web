16834b25ad47eba37565b9dcbf2e267b4dc14c42
Chaiyapat Oam <chaiyapat.meey@mail.kmutt.ac.th>
2024-04-21 15:46:05 +0700

    feat: check sign in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,51 @@ import { Button } from '@/components/common/button';
 import { NavigationBar } from '@/components/common/navigation-bar';
 import { PageLayout } from '@/components/common/page-layout';
 import { Separator } from '@/components/common/separator';
+import { UserProfileDropdown } from '@/components/common/user-profile-dropdown';
+import { useAuth } from '@/hooks/auth-hook';
 import { RoutePath } from '@/libs/constants';
 import { ChevronRightIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export default function IndexPage() {
+  const { user } = useAuth();
+
   return (
     <PageLayout>
       <NavigationBar>
         <div className="flex flex-row items-center space-x-2">
-          <Button
-            variant="ghost"
-            size="sm"
-            asChild
-          >
-            <Link to={RoutePath.SIGNIN}>Sign in</Link>
-          </Button>
-          <Button
-            size="sm"
-            asChild
-          >
-            <Link to={RoutePath.SIGNUP}>Sign up</Link>
-          </Button>
+          {user.data ? (
+            <>
+              <Button
+                variant="outline"
+                size="sm"
+                asChild
+              >
+                <Link to={RoutePath.DASHBOARD}>Dashboard</Link>
+              </Button>
+              <UserProfileDropdown
+                displayName={user.data.displayName}
+                email={user.data.email}
+                accountType={user.data.accountType}
+              />
+            </>
+          ) : (
+            <>
+              <Button
+                variant="ghost"
+                size="sm"
+                asChild
+              >
+                <Link to={RoutePath.SIGNIN}>Sign in</Link>
+              </Button>
+              <Button
+                size="sm"
+                asChild
+              >
+                <Link to={RoutePath.SIGNUP}>Sign up</Link>
+              </Button>
+            </>
+          )}
         </div>
       </NavigationBar>
       <div className="container flex flex-col items-center justify-center py-32">
